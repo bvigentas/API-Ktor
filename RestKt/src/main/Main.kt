bd98c0b6d701846079526f14d8ca0dd516cba6f3
Gabriel Schneider <gabriel.schneider88@hotmail.com>
2019-05-24 10:18:49 -0300

    Creating a Ktor application

--- RestKt/src/main/Main.kt
+++ RestKt/src/main/Main.kt
@@ -1,5 +1,22 @@
 package main
 
+import io.ktor
+
+fun Application.module() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(ContentNegotiation) {
+        jackson {
+            configure(SerializationFeature.INDENT_OUTPUT, true)
+        }
+    }
+
+    install(Routing) {
+        widget(WidgetService())
+    }
+}
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("The application has started")
+    embeddedServer(Netty, 8080, module = Application::module).start()
 }
