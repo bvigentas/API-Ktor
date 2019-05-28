b3731d62f5a95e3c8052f41369e2e002ab4d51ba
Bruno Vigentas <brunovigentas@gmail.com>
2019-05-27 23:31:26 -0300

    Suprimido primary keys

--- src/br/furb/model/Comandas.kt
+++ src/br/furb/model/Comandas.kt
@@ -1,9 +1,8 @@
 package br.furb.ktorAPI.br.furb.model
 
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.dao.IntIdTable
 
-object Comandas : Table() {
-    val id = integer("id").primaryKey().autoIncrement()
+object Comandas : IntIdTable() {
     val idUsuario = (integer("id_usuario") references Usuarios.id)
     val produtos = varchar("produtos", 255)
     val valorTotal = decimal("valor_total", 2, 2)
--- src/br/furb/model/Usuarios.kt
+++ src/br/furb/model/Usuarios.kt
@@ -1,9 +1,8 @@
 package br.furb.ktorAPI.br.furb.model
 
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.dao.IntIdTable
 
-object Usuarios : Table(){
-    val id = integer("id").primaryKey().autoIncrement()
+object Usuarios : IntIdTable(){git
     val email = varchar("nome",255)
     val senha = varchar("senha", 255)
 }
